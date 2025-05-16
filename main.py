a4b742d92837df791fa5e051ad4063f0f0872b69
ManuelR11 <rod21509@uvg.edu.gt>
2025-05-15 18:14:11 -0600

    Correcciones

--- main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI, HTTPException
-from backend.database.schemas import Usuario, Mensaje, Grupo, Blockchain
-from sqlalchemy.orm import Session
-
-# Crear la instancia de FastAPI
-app = FastAPI()
