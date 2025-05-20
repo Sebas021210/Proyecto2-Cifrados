f3f929bba8a779b55292befdd2dc9db9e02f89fe
Sebastián Solorzano <sol21826@uvg.edu.gt>
2025-05-20 16:47:45 -0600

    Ruta de mensajes individuales

--- backend/routes/mensajes.py
+++ backend/routes/mensajes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
 from sqlalchemy.exc import IntegrityError
-
 from backend.models.responses import SuccessfulLoginResponse, SuccessfulRegisterResponse
 from backend.models.user import UserBase, LoginRequest
 from backend.database import db, User
@@ -10,3 +10,18 @@ from backend.controllers.auth import (
     get_current_user,
 )
 from backend.controllers.keys import generate_rsa_keys, generate_ecc_keys
+from backend.controllers.messages import guardar_mensaje_individual
+from backend.models.message import MessageIndidualRequest, MessageIndividualResponse
+
+router = APIRouter()
+
+@router.post("/message")
+def send_message(message_request: MessageIndidualRequest):
+    try:
+        result = guardar_mensaje_individual(message_request)
+        return result
+    except Exception as e:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Error saving message: {e}",
+        )
