054eb78c17708b93b88844b37552fdc035300733
Sebastián Solorzano <sol21826@uvg.edu.gt>
2025-06-03 19:35:12 -0600

    Corrección al recibir mensajes

--- backend/routes/mensajes.py
+++ backend/routes/mensajes.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
 from backend.database import db, User, Mensajes, get_db
-from backend.controllers.messages import guardar_mensaje_individual, verificar_y_descifrar_mensaje
+from backend.controllers.messages import guardar_mensaje_individual
 from backend.utils.auth import get_current_user
 from backend.controllers.firma import calcular_hash_mensaje, decrypt_message_aes
 from backend.models.message import MessageIndividualResponse, MessageReceived, MessageIndividualRequestSimplified
 from sqlalchemy.orm import Session
 from types import SimpleNamespace as Namespace
 from typing import List
-import json, base64
+import json
 
 router = APIRouter()
 
@@ -69,37 +69,22 @@ def send_individual_message(
 @router.get("/message/received", response_model=List[MessageReceived])
 def get_received_messages(
     db: Session = Depends(get_db),
-    user: User = Depends(get_current_user),
-    clave_privada_b64: str = Query(..., description="Clave privada del receptor en formato Base64 URL-safe"),
-    algoritmo_hash: str = Query("sha256")
+    user: User = Depends(get_current_user)
 ):
-    try:
-        clave_privada_bytes = base64.urlsafe_b64decode(clave_privada_b64)
-        clave_privada = clave_privada_bytes.decode("utf-8")
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Error al decodificar clave privada: {str(e)}")
+    messages = (
+        db.query(Mensajes)
+        .filter(Mensajes.id_receptor == user.id_pk)
+        .order_by(Mensajes.timestamp.desc())
+        .all()
+    )
 
-    messages = db.query(Mensajes).filter(Mensajes.id_receptor == user.id_pk).order_by(Mensajes.timestamp.desc()).all()
     message_responses = []
 
     for msg in messages:
-        try:
-            mensaje_descifrado = verificar_y_descifrar_mensaje(
-                mensaje_cifrado=json.loads(msg.mensaje),
-                clave_aes_cifrada=json.loads(msg.clave_aes_cifrada),
-                firma=msg.firma,
-                hash_mensaje=msg.hash_mensaje,
-                clave_privada_receptor_pem=clave_privada,
-                clave_publica_remitente_pem=msg.remitente.public_key,
-                algoritmo_hash=algoritmo_hash
-            )
-        except Exception as e:
-            mensaje_descifrado = f"ERROR: {str(e)}"
-
         message_responses.append(
             MessageReceived(
                 id=msg.id,
-                message=mensaje_descifrado,
+                message=msg.mensaje,
                 firma=msg.firma,
                 hash_mensaje=msg.hash_mensaje,
                 clave_aes_cifrada=msg.clave_aes_cifrada,
